cdc2f442ee4707a6e384a697c15b691f15769469
Jack.Yj.Din <jack.yj.din@benq.com>
2016-05-14 18:47:27 +0800

    Initial Commit

---
+++ HW11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW11
+//
+//  Created by Spade Jack on 5/14/16.
+//  Copyright © 2016 galileo.jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
